21ad052094b9bf0743618cd3ce2554d8b12b1573
Erik Michael <erikmichael42@gmail.com>
2020-12-18 02:37:15 +0000

    Discord bot now will send url of song name to discord server

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+#Returns top search on YouTube for the provided song name
+#For best result, make sure to spell correctly, add lyrics to end of title
+
 import discord
 import os
+from youtubesearchpython import SearchVideos
+
 
 client = discord.Client()
 
@@ -12,7 +17,12 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  if message.content.startswith('$go'):
+    userInput = message.content[3:]
+    videosSearch = SearchVideos(userInput, offset = 1, mode = "dict", max_results=1)
+    listy = videosSearch.result()['search_result']
+    diction = listy[0]
+    link = diction["link"]
+    await message.channel.send(link)
 
 client.run(os.getenv('TOKEN'))
